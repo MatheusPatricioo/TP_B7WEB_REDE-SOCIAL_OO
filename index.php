fe9d64ea4caca22afade9dd9ca9b69eab65f95ac
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-08-20 13:33:48 -0300

    up

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require'models/Auth.php';
 
 //tudo oq verifica bando de dados eu tenho que passar o PDO
 //passei o pdo e a base, pq se n achar, ele manda pra base.
-$auth = new Auth($pdo, $base);$auth = new Auth($pdo, $base);
+$auth = new Auth($pdo, $base);
 =======
 <?php
 require 'config.php';
