789f0b27d717c7180928995432d00aa1d327f373
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-09-04 13:18:09 -0300

    update
    att, corrigi bugs e linkei as páginas com o banco de dados.

--- index.php
+++ index.php
@@ -2,19 +2,48 @@
 require 'config.php';
 require 'models/Auth.php';
 
-//tudo oq verifica bando de dados eu tenho que passar o PDO
+//tudo oq verifica banco de dados eu tenho que passar o PDO
 //passei o pdo e a base, pq se n achar, ele manda pra base.
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
 
-$activeMenu = 'fotos'; // agr qnd ta na page home, msm sem passar o mouse por cima, ele acende com um destaque
+$activeMenu = 'home'; // agr qnd ta na page home, msm sem passar o mouse por cima, ele acende com um destaque
 
 require 'partials/header.php';
 require 'partials/menu.php';
 ?>
 <section class="feed mt-10">
-  
-...
+  <div class="row">
+    <div class="column pr-5">
+
+      <?php require 'partials/feed-editor.php' ?>
+
+
+
+
+    </div> 
+    <div class="column side pl-5">
+      <div class="box banners">
+        <div class="box-header">
+          <div class="box-header-text">Patrocinios</div>
+          <div class="box-header-buttons">
+
+          </div>
+        </div>
+        <div class="box-body">
+          <a href=""><img src="https://alunos.b7web.com.br/media/courses/php-nivel-1.jpg" /></a>
+          <a href=""><img src="https://alunos.b7web.com.br/media/courses/laravel-nivel-1.jpg" /></a>
+        </div>
+      </div>
+      <div class="box">
+        <div class="box-body m-10">
+          Criado com ❤️ por Patricio.ltda
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 </section>
 <?php
 require 'partials/footer.php';
--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,7 @@
+<?php
+$firstName = current(explode(' ', $userInfo->name));
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -5,7 +9,7 @@
   <meta charset="utf-8" />
   <title></title>
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
-  <link rel="stylesheet" href=" <?= $base; ?>/assets/css/style.css" />
+  <link rel="stylesheet" href="<?= $base; ?>/assets/css/style.css" />
 </head>
 
 <body>
