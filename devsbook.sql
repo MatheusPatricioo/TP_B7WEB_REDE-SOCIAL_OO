500323e7eb66ea22448f9eead2002d442282e93f
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-09-05 11:43:43 -0300

    update
    atualização de código

---
+++ devsbook.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05/09/2024 às 14:58
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `devsbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `postcomments`
+--
+
+CREATE TABLE `postcomments` (
+  `id` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `postlikes`
+--
+
+CREATE TABLE `postlikes` (
+  `id` int(11) NOT NULL,
+  `id_post` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `userrelations`
+--
+
+CREATE TABLE `userrelations` (
+  `id` int(11) NOT NULL,
+  `user_from` int(11) NOT NULL,
+  `user_to` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `birthdate` date NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `work` varchar(100) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `cover` varchar(100) NOT NULL,
+  `token` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `name`, `birthdate`, `city`, `work`, `avatar`, `cover`, `token`) VALUES
+(1, 'mp.matheuspatricio000@gmail.com', '$2y$10$7BWQHVdKcYOB/qXjCbDdgeYp/1sUuhfa37QjAeqZyN/rRWpO2jduu', 'matheus', '1996-06-18', '', '', '', '', 'da90070ff5752cae2a4c41bb4a95ee72'),
+(2, 'teste13@testando.com', '$2y$10$xXxL1qfmowwuM1yTlzOXKeviNxVnskGYKrjSFvJuHEx18lgmjOBIS', 'carecationFuracão', '1995-06-18', '', '', NULL, '', 'a98b62f9e13501622e6dbe1c31d10c86'),
+(3, 'luizaatendimento@edutec.com', '$2y$10$rGbkikqJRpWrQ/ucAlULA.I6kg4VotcXbr9ulG.6eBqud0jr1tq1i', 'luiza', '1996-09-18', '', '', '', '', 'b2a7707dffc0cb5ea3227d0a28b14333');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `postcomments`
+--
+ALTER TABLE `postcomments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `postlikes`
+--
+ALTER TABLE `postlikes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `userrelations`
+--
+ALTER TABLE `userrelations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `postcomments`
+--
+ALTER TABLE `postcomments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `postlikes`
+--
+ALTER TABLE `postlikes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `userrelations`
+--
+ALTER TABLE `userrelations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
